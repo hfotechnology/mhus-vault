edbaebd1e1be99884c25894a34957225eaf2d058
Mike Hummel <mike.hummel@billingsolutions.de>
2021-10-05 16:06:01 +0200

    7.5.0

--- vault-rest/src/main/java/de/mhus/app/vault/rest/VaultNodeTransformer.java
+++ vault-rest/src/main/java/de/mhus/app/vault/rest/VaultNodeTransformer.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2018 Mike Hummel (mh@mhus.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.mhus.app.vault.rest;
 
 import de.mhus.app.vault.api.CherryVaultApi;
@@ -11,5 +26,4 @@ public class VaultNodeTransformer extends PojoTransformer {
     protected PojoModelFactory getPojoModelFactory() {
         return M.l(CherryVaultApi.class).getManager().getPojoModelFactory();
     }
-
 }
